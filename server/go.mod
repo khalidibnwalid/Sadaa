6023def09a4070c51e7b3de7992be3761ead3e7f
khalidibnwalid <kh.hawajreh@gmail.com>
2025-08-01 23:56:30 +0300

    feat(graphql): initialize graphql - Installed github.com/99designs/gqlgen - Created gqlgen.yml - Run `go run github.com/99designs/gqlgen generate` - Added graphql handlers to the router with playground

--- server/go.mod
+++ server/go.mod
@@ -3,14 +3,21 @@ module github.com/khalidibnwalid/sadaa/server
 go 1.24.4
 
 require (
+	github.com/99designs/gqlgen v0.17.78
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
+	github.com/vektah/gqlparser/v2 v2.5.30
 )
 
 require (
+	github.com/agnivade/levenshtein v1.2.1 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
 )
--- server/internal/router/routes.go
+++ server/internal/router/routes.go
@@ -3,7 +3,16 @@ package router
 import (
 	"net/http"
 
+	"github.com/khalidibnwalid/sadaa/server/internal/graph"
 	"github.com/khalidibnwalid/sadaa/server/internal/middleware"
+	"github.com/khalidibnwalid/sadaa/server/internal/resolvers"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/vektah/gqlparser/v2/ast"
 )
 
 func (r *Router) SetupRoutes() {
@@ -25,5 +34,27 @@ func (r *Router) SetupRoutes() {
 			w.Write([]byte("pong"))
 		})
 
+		r.HandleFunc("/gql", graphqlHandler().ServeHTTP)
+		if r.Server.Config.Environment == "development" {
+			r.Handle("/gql/pg", playground.Handler("GraphQL playground", "/query"))
+		}
+
 	})
+}
+
+func graphqlHandler() http.Handler {
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &resolvers.Resolver{}}))
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+
+	return srv
 }
